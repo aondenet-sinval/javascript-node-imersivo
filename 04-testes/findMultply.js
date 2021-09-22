c6a65d907a63f5e430b2d15303f7d796c9d5f1cc
aondenet-sinval <sinvalsatv@gmail.com>
2021-09-22 18:51:42 -0300

    up

--- 04-testes/findMultply.js
+++
@@ -1,7 +0,0 @@
-const should = require('should')
-
-function findMultply(a,b){
-	return a * b;
-}
-
-module.exports = findMultply;
--- 04-testes/findSoma.js
+++
@@ -1,7 +0,0 @@
-const should = require('should')
-
-function findSoma(a,b){
-	return a+b;
-}
-
-module.exports = findSoma;
